fc38e11609ee7ffe7c4f2c33f601f41f1bd3c15a
tugkandemirtas <tugkandemirtas06@gmail.com>
2026-01-22 13:25:46 +0300

    Add files via upload
    İn this code number which you enter hexadecimal code converted to binary.

---
+++ hexa-to-binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    char hex[32];
+
+    printf("Hexadecimal sayi giriniz: ");
+    scanf("%s", hex);
+
+    printf("Binary: ");
+
+    for (int i = 0; hex[i] != '\0'; i++) {
+        switch (hex[i]) {
+            case '0': printf("0000"); break;
+            case '1': printf("0001"); break;
+            case '2': printf("0010"); break;
+            case '3': printf("0011"); break;
+            case '4': printf("0100"); break;
+            case '5': printf("0101"); break;
+            case '6': printf("0110"); break;
+            case '7': printf("0111"); break;
+            case '8': printf("1000"); break;
+            case '9': printf("1001"); break;
+            case 'A': case 'a': printf("1010"); break;
+            case 'B': case 'b': printf("1011"); break;
+            case 'C': case 'c': printf("1100"); break;
+            case 'D': case 'd': printf("1101"); break;
+            case 'E': case 'e': printf("1110"); break;
+            case 'F': case 'f': printf("1111"); break;
+            default:
+                printf("\nHata: Gecersiz hexadecimal karakter!\n");
+                return 1;
+        }
+    }
+
+    return 0;
+}
